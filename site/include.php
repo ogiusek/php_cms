dc501abefdd7476615c75eff44845286435d4828
ogius <kowalewski.olgierd@gmail.com>
2024-02-17 00:13:24 +0100

    wrote first working components version
    added main index.html file where main html is rendered

--- site/include.php
+++ site/include.php
@@ -1,21 +1,14 @@
 <?php
   require_once dirname(realpath(__DIR__)) . '/include/include.php';
-  require_once "site/display_site.php";
+
+  require_once "lib/include.php";         // user functions
+  require_once "components/include.php";  // user components
 
   ob_start();
-  // require_once "lib/include.php";
-  // require_once "components/include.php";
-  // require_once "pages/include.php";
-  echo "<h1>example site</h1>";
-  echo "<style>h1{background-color: red;}</style>";
-  echo "<style>div{background-color: green;}</style>";
-  echo "<style>div{color: red;}</style>";
-  echo "<style>div{padding: 10px;}</style>";
-  echo "<style>h1{color: blue;}</style>";
-  
+  // $scss
+  display_component(); // renders example component
+  // require_once "pages/include.php"; // renders current page
   $app_content = ob_get_clean();
   
   $html = file_get_contents(__DIR__ . "/index.html");
   echo display_site($html, $app_content);
-  
-  // echo $res;
