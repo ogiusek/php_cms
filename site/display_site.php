7a687292a36fa01bb4ce893a39959493e5a5d3ad
ogius <kowalewski.olgierd@gmail.com>
2024-02-17 00:03:12 +0100

    added display site code

--- site/display_site.php
+++
@@ -1,7 +0,0 @@
-<?php
-  require_once "site/display_site/index.php";
-  function display_site($html, $app_content) {
-    $html = display_site\replace_app_with_app_content($html, $app_content);
-    $html = display_site\move_all_tags($html);
-    return $html;
-  }
