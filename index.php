d99780ac2805089bb46a9e53c44ee27763778ad5
ogius <kowalewski.olgierd@gmail.com>
2024-02-17 00:10:20 +0100

    created main file

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  session_start();
+
+  require_once "site/include.php";
+  // $_SERVER['REQUEST_URI'];
+
