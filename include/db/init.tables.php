341d872945a0d41165ff8fdda05b13cfdaa93503
ogius <kowalewski.olgierd@gmail.com>
2024-02-18 20:22:25 +0100

    created users tables

--- include/db/init.tables.php
+++ include/db/init.tables.php
@@ -1,4 +1,29 @@
 <?php
-  require_once "config.php";
+require_once "config.php";
 
-  // create tables
+// create tables
+
+$tables = [
+  "CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) UNIQUE NOT NULL,
+    `hash` VARCHAR(256)
+  )",
+  "CREATE TABLE IF NOT EXISTS `waiting_users` (
+    `uuid` VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    `email` VARCHAR(255) UNIQUE NOT NULL
+  )",
+  "CREATE TABLE IF NOT EXISTS `users_sessions` (
+    `uuid` VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    `user_id` INT NOT NULL,
+    `session_start` DATETIME DEFAULT CURRENT_TIMESTAMP
+  )",
+];
+
+foreach ($tables as $table) {
+  $db = &$GLOBALS["db"];
+  if(($db->query($table)) == false){
+    echo "Error creating table: " . $db->error;
+    exit(1);
+  }
+}
