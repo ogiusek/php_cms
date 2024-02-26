3c6fc1e9dc502ed500ec864d18a6c848228718c7
ogius <kowalewski.olgierd@gmail.com>
2024-02-26 20:47:57 +0100

    moved env

--- application/index.php
+++ application/index.php
@@ -1,4 +1,9 @@
 <?php
+if($_ENV['DEBUG']) {
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+}
 session_start();
 require_once "include/include.php";
 
