7eea47197bdbe82baef97e1afb9b4be273c9a20b
ogius <kowalewski.olgierd@gmail.com>
2024-03-27 11:41:46 +0100

    added head class

--- application/include/lib/head/namespace.php
+++ application/include/lib/head/namespace.php
@@ -16,39 +16,32 @@ class IHead {
     echo "<head>$head</head>";
   }
 
-  public string $icon = "";
+  public string $icon = "-";
   public function icon(string $icon): self {
-    // $this->head .= "<link rel=\"icon\" href=\"$icon\">";
     $this->icon = $icon;
     return $this;
   }
 
-  public string $image = "";
+  public string $image = "-";
   public function image(string $image): self {
-    // $this->head .= "<meta property=\"og:image\" content=\"$image\">";
     $this->image = $image;
     return $this;
   }
 
-  public string $title = "";
+  public string $title = "-";
   public function title(string $title): self {
-    // $this->head .= "<meta property=\"og:title\" content=\"$title\">";
-    // $this->head .= "<title>$title</title>";
     $this->title = $title;
     return $this;
   }
 
-  public string $description = "";
+  public string $description = "-";
   public function description(string $description): self {
-    // $this->head .= "<meta name=\"description\" content=\"$description\">";
-    // $this->head .= "<meta property=\"og:description\" content=\"$description\">";
     $this->description = $description;
     return $this;
   }
 
-  public string $keywords = "";
+  public string $keywords = "-";
   public function keywords(string $keywords): self {
-    // $this->head .= "<meta name=\"keywords\" content=\"$keywords\">";
     $this->keywords = $keywords;
     return $this;
   }
