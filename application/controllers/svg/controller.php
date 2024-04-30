722c7285b8fe062b96891357a9fc9c048e7e76c3
ogius <kowalewski.olgierd@gmail.com>
2024-04-30 10:33:14 +0200

    edited svg controller

--- application/controllers/svg/controller.php
+++ application/controllers/svg/controller.php
@@ -2,7 +2,22 @@
 \request\verify()
   ->require_url_params(["url", "color"]);
 
+function is_svg($file) {
+  return preg_match('/\.svg$/i', $file);
+}
+
 $file = $_ENV['STATIC'].$_GET['url'];
+if(!file_exists($file)){
+  header("HTTP/1.0 404 Not Found");
+  exit();
+}
+// if not svg just send it
+if(!is_svg($file)){
+  $extension = pathinfo($file, PATHINFO_EXTENSION);
+  header("Content-Type: image/$extension");
+  echo file_get_contents($file);
+  exit();
+}
 $svg = @file_get_contents($file);
 header("Content-Type: image/svg+xml");
 $color = $_GET['color'];
