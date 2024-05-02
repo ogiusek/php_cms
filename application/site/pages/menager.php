5313a6a33ffedba5ecf1113194af84aeccec6e6e
ogius <kowalewski.olgierd@gmail.com>
2024-05-02 12:16:56 +0200

    added site menager

--- application/site/pages/menager.php
+++ application/site/pages/menager.php
@@ -6,7 +6,3 @@ foreach($page_contents as $page_content){
   echo \components()
     ->render($page_content['content']);
 }
-?>
-<!-- <textarea rows="10" cols="80">{\\rtf1\\ansi\\ansicpg1252\\deff0\\deflang1033{\\fonttbl{\\f0\\fnil\\fcharset0 Arial;}}\n
-\\viewkind4\\uc1\\pard\\f0\\fs24 This is some editable RTF content in HTML.\\par\n
-}</textarea> -->
