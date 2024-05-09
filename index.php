f503321ca9c252c622b511ca9dd99652f7d5be35
ogius <kowalewski.olgierd@gmail.com>
2024-05-09 10:33:48 +0200

    updated env file

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 // echo phpversion(); // requires PHP >= 8.1
-$_ENV["STATIC"] = __DIR__."/static";
 if(file_exists(__DIR__."/env.php"))
   include "env.php";
+$_ENV["STATIC"] = __DIR__."/static/";
 require_once "application/index.php";
 
-// if you do not have account create one with line below
-if(
-  isset($_ENV["ROOT_EMAIL"]) &&
-  isset($_ENV["ROOT_PASSWORD"])
-)
+
+// create root account
+if(isset($_ENV["ROOT_EMAIL"]) &&
+   isset($_ENV["ROOT_PASSWORD"]))
   \db\user\create_account(
     $_ENV["ROOT_EMAIL"],
     $_ENV["ROOT_PASSWORD"]
   );
-  // \db\user\create_account("try@gmail.com", "password"); // <----------------------------------------------
