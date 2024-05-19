f8023ecd8748405016acc9542379c8f212eaa23c
ogius <kowalewski.olgierd@gmail.com>
2024-05-20 00:16:45 +0200

    updated page redirector

--- application/site/pages/menager.php
+++ application/site/pages/menager.php
@@ -1,8 +1,12 @@
 <?php
-$page_id = $GLOBALS['page_id'];
+$page_id = $_SESSION['page_id'];
 $page_contents = \db\pages\content\get_by_page_id($page_id);
 
 foreach($page_contents as $page_content){
   echo \components()
     ->render($page_content['content']);
 }
+
+// use Jefs42\LibreTranslate;
+// $translator = new LibreTranslate();
+// echo $translator->translate("Hello World", "en", "es");
